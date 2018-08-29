2bb7b01072fe54276e798a5978f3164be7fca242
Munish Saini <munishsainicr@gmail.com>
2018-08-29 15:57:33 -0400

    com

--- src/main/java/app/MsdPetClinicApplication.java
+++ src/main/java/app/MsdPetClinicApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsdPetClinicApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MsdPetClinicApplication.class, args);
 	}
